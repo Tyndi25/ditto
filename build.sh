5aa3d65fd2f74c93f891f86f21df108c22f6163b
Sargun Vohra <sargun.vohra@gmail.com>
2018-09-14 20:17:55 -0700

    simplify build

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ set -x
 
 yarn install
 yarn run netlify-lambda build functions
-
-poetry install --no-dev
-poetry run ditto transform --base-url='https://pokeapi-prod.netlify.com/' --dest-dir='_dist'
